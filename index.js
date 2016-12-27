ab2a5085afb057cef05e631a50cb07cbf8104580
Bernat Jufré <info@behind.design>
2016-12-27 01:02:43 +0100

    Allow passing null as a

--- index.js
+++ index.js
@@ -3,13 +3,19 @@
 const Koa = require('koa');
 const isPlainObject = require('lodash.isplainobject');
 
+/**
+ * Set in context middleware for Koa 2 application.
+ */
 module.exports = function setInContextMiddleware(props, value) {
 
-  if (typeof props === 'string' && !value) {
-    throw new Error(`When passing a String as 'props', you must pass a value for to set it in the 'ctx.state'.`);
-  }
-
-  if (Array.isArray(props) && (isPlainObject(props[0]) && !(props[0].name) || !(props[0].value))) {
+  /**
+   * We have an array of objects.
+   * Check that the object have the correct form.
+   * With the properties "name and value".
+   * If any of the objects doesn't comply with the form,
+   * throw an error.
+   */
+  if (Array.isArray(props) && !(props.every(isObjectAndHasCorrectForm))) {
     throw new Error(`
       When passing an array of objects they must in the form of:
         [{
@@ -19,7 +25,11 @@ module.exports = function setInContextMiddleware(props, value) {
     `);
   }
 
-  if (isPlainObject(props) && (!(props.name) || !(props.value))) {
+  /**
+   * If we only have an object, check that it has the correct form.
+   * "name and value". If not throw an error.
+   */
+  if (isObjectAndHasCorrectForm(props)) {
     throw new Error(`
       When passing an object it must be in the form of:
         {
@@ -29,13 +39,33 @@ module.exports = function setInContextMiddleware(props, value) {
     `);
   }
 
+  /**
+   * Return the actual middleware.
+   */
   return function setInContext(ctx, next) {
-    _setPropertiesToState(ctx.state, props, value);
+    setPropertiesIntoState(ctx.state, props, value);
     return next();
   };
 };
 
-function _setPropertiesToState(state, props, value) {
+/**
+ * setPropertiesIntoState: Actual function that normalizes the parameters
+ * that the user passes so we can work with easely.
+ *
+ * If a string is passed, create an array with only one object with the form
+ *  'key', 'value' => [{ name: 'key', value: 'value' }]
+ *
+ * If an object is given, just wrap it in an array.
+ * { name: 'key', value: 'value' } => [{ name: 'key', value: 'value' }]
+ *
+ * If an array of objects is given, just continue.
+ *
+ * @param Object state The "context.state" of the request.
+ * @param mixed props  The key|value to set in the context.
+ * @param mixed value  The value to set in the context, if "props" is a string.
+ * @return void
+ */
+function setPropertiesIntoState(state, props, value) {
   let _props;
 
   if (typeof props === 'string') {
@@ -56,4 +86,17 @@ function _setPropertiesToState(state, props, value) {
   _props.forEach(prop => {
     state[prop.name] = prop.value;
   });
+}
+
+/**
+ * isObjectAndHasCorrectForm: Check whether "obj" is
+ * plain object with the form: { name: 'key', value: 'value' }.
+ *
+ * @param mixed obj The value to check.
+ * @return Boolean
+ */
+function isObjectAndHasCorrectForm(obj) {
+  return isPlainObject(obj) &&
+    obj.hasOwnProperty('name') &&
+    obj.hasOwnProperty('value');
 }
